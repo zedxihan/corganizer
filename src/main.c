eae9e91c14f7a3249210e7ca52b23b918c0592b1
Gaffar Mahmud <dev@gaffarmahmud.com>
2026-04-19 16:59:31 +0600

    feat: integrate execute module and implement file organization logic in main

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include "corganizer.h"
+
 #include <stdio.h>
 #include <string.h>
 
@@ -7,8 +8,8 @@ int main(int argc, char *argv[]) {
     printf("Usage: ./corganizer <scan | run> <folder>\n");
     return 1;
   }
-  if (strcmp(argv[1], "scan") != 0 && strcmp(argv[1], "run") != 0) {
 
+  if (strcmp(argv[1], "scan") != 0 && strcmp(argv[1], "run") != 0) {
     printf("Error: Mode must be 'scan' or 'run'\n");
     return 1;
   }
@@ -20,10 +21,21 @@ int main(int argc, char *argv[]) {
 
   printf("Mode: %s\n\n", argv[1]);
 
+  // function calls
+  FileEntry *list = scan_directory(argv[2]);
+
+  if (!list) {
+    printf("No files to organize or unable to read directory\n");
+    return 0;
+  }
+
   if (strcmp(argv[1], "scan") == 0)
-    scan_directory(argv[2]);
+    preview_moves(list);
   else
-    organize_directory(argv[2]);
+    execute_moves(list);
+
+  // free memory
+  free_list(list);
 
   return 0;
 }
